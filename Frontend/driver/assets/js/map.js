acc125633e61b1ef4788f18843fe6be11c286036
Archita Trivedi <archita.ms11@gmail.com>
2026-08-08 15:04:09 +0530

    feat: update RoadSafe AI Driver module

--- Frontend/driver/assets/js/map.js
+++
@@ -1,9 +0,0 @@
-/* global MapEngine */
-/**
- * Legacy map script alias redirecting to MapEngine
- */
-if (typeof window !== 'undefined' && typeof MapEngine !== 'undefined') {
-    window.initMap = function() {
-        MapEngine.init('map');
-    };
-}
